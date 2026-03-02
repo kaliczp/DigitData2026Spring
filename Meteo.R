ad0b7b0e4765b8695021e80edec54e310edc5c24
Péter Kalicz <kaliczp@gmail.com>
2026-03-02 12:55:05 +0100

    Get info from object

--- Meteo.R
+++ Meteo.R
@@ -5,3 +5,7 @@ read.table("ta_h_Turkeve_19012023.csv", sep = ";") # Impot csv
 ?read.table # Manual page
 Turk <- read.table("ta_h_Turkeve_19012023.csv", sep = ";") # Save in an object
 save.image() # Save actual environment
+
+str(Turk) # Structure of object
+head(Turk) # Header of object
+tail(Turk) # End of object
