c89a82643058bba1202a81e429905286bd173b66
Péter Kalicz <kaliczp@gmail.com>
2026-04-10 10:56:53 +0200

    Automatic read and some manipulation for yearly sum

--- CsapadékAut.R
+++ CsapadékAut.R
@@ -29,3 +29,25 @@ dev.off()
 png(units = "cm", height = 10, width = 19, res = 300)
 plot(Ido,raw$r, type = "h", col = "blue", ylab = "Napi csapadék [mm]", xaxs = "i", ylim = c(0,80), yaxs = "i", lend = 1)
 dev.off()
+
+library(xts)
+## Automatizálás felé
+rawinput <- "HABP_1RD_20020101_20251231_45604.csv"
+rawinput <- "HABP_1RD_20260101_20260228_45604.csv"
+
+## HABP karaktersort tartalmazó fájlok beolvasása
+for(rawinput in dir(pattern = "HABP")) {
+    raw <- read.table(rawinput, header = TRUE, sep = ";")
+    Ido <- as.Date(paste(substring(as.character(raw$Time),1,4),
+                         substring(as.character(raw$Time),5,6),
+                         substring(as.character(raw$Time),7,8),
+                         sep = "-"))
+    assign(sub("csv", "xts", rawinput),xts(raw$r, Ido))
+}
+
+Kunpeszér <- c(HABP_1RD_20020101_20251231_45604.xts,
+               HABP_1RD_20260101_20260228_45604.xts)
+plot(Kunpeszér)
+
+## Éves összeg
+plot(apply.yearly(Kunpeszér, sum))
