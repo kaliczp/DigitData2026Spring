2166817c7cd84d99097da31c40df5c78f2d81879
Péter Kalicz <kaliczp@gmail.com>
2026-04-10 09:48:37 +0200

    Adat indexelés

--- CsapadékAut.R
+++ CsapadékAut.R
@@ -1,2 +1,7 @@
-# Első állomás teszt beolvasás
-raw <- read.table("HABP_1RD_20020101_20251231_45604.csv", header = TRUE, sep = ";")
+## Első állomás teszt beolvasás
+rawinput <- "HABP_1RD_20020101_20251231_45604.csv"
+raw <- read.table(rawinput, header = TRUE, sep = ";")
+## Adat indexelés
+raw$r
+raw[1,"r"]
+raw[1:7, 3]
